784c241c8e6dc072ccb0fcdc106292563ae5dfd5
vbox <vbox@vbox.myguest.virtualbox.org>
2023-09-20 18:11:54 +0900

    commit 3

--- 20210800_kimhyojoon.sh
+++ 20210800_kimhyojoon.sh
@@ -1,15 +1,21 @@
 #!bin/sh/
-if [ $1 -gt 0 ] && [ $2 -gt 0 ]
+if [ "$#" -ne 2 ]
 then
-	for i in $(seq 1 $1)
-	do
-	    for j in $(seq 1 $2)
-	    do
-	    	ans=`expr $i \* $j`
-	    	echo "$1 * $2 = $ans"
-	    done
-	done
+	echo "Invalid input"
 else
-	echo 'Input must be greater than 0'
+	if [ $1 -gt 0 ] && [ $2 -gt 0 ]
+	then
+		for i in $(seq 1 $1)
+		do
+		    ans=""
+		    for j in $(seq 1 $2)
+		    do
+		    	ans="$ans$i * $j = `expr $i \* $j`    "
+		    done
+		    echo "$ans"
+		done
+	else
+		echo 'Input must be greater than 0'
+	fi
 fi
 exit 0
